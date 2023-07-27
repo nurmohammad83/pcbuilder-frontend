7099996f337284e9f78e536608edb51f0f7ef242
Nur Mohammad <mohammadnur5480@gmail.com>
2023-07-28 00:20:18 +0600

    Create Root layout

--- src/components/Layout/RootLayout.jsx
+++ src/components/Layout/RootLayout.jsx
@@ -1,6 +1,13 @@
-const RootLayout = () => {
+import AppFooter from "./AppFooter"
+import AppHeader from "./AppHeader"
+
+const RootLayout = ({children}) => {
   return (
-    <div>RootLayout</div>
+    <div>
+        <AppHeader />
+        {children}
+        <AppFooter />
+    </div>
   )
 }
 export default RootLayout
