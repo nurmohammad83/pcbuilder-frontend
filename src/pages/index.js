77d02c9c8dee88b61ddf1d5b286aa581678819e6
Nur Mohammad <mohammadnur5480@gmail.com>
2023-07-28 22:14:12 +0600

    Create Mypc build page for added pc component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,40 +4,38 @@ import FeaturedProducts from "@/components/UI/FeaturedProducts"
 import axios from "axios"
 import Head from "next/head"
 
-const HomePage = ({products}) => {
+const HomePage = ({ products }) => {
   return (
     <div>
-    <Head>
-      <title>Home</title>
-      <meta
-        name="description"
-        content="This is pc builder site  made by next-js"
-      />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <FeaturedCategory />
-    <FeaturedProducts products={products}/>
-  </div>
+      <Head>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="This is pc builder site  made by next-js"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <FeaturedCategory />
+      <FeaturedProducts products={products} />
+    </div>
   )
 }
 export default HomePage
-
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>
 }
 
-
 export async function getStaticProps() {
 
-  const apiUrl = 'https://product-crud-server.vercel.app/products'; 
+  const apiUrl = 'https://product-crud-server.vercel.app/products';
   try {
     const response = await axios.get(apiUrl);
-    const products = response.data; 
+    const products = response.data;
     return {
       props: {
-        products:products
+        products: products
       },
       revalidate: 3600, // 1 hour (3600 seconds)
     };
--- src/pages/pc_builder.js
+++ src/pages/pc_builder.js
@@ -1,72 +1,18 @@
-import React, { useState } from 'react';
-import { Layout, Select, Button, Card } from 'antd';
+import React from 'react';
 import RootLayout from '@/components/Layout/RootLayout';
-import { featuredCategories } from '@/utils';
-import Link from 'next/link';
-
-const { Option } = Select;
-const { Meta } = Card;
-
+import MyPcBuild from '@/components/UI/MyPcBuild';
 
 const PCBuildPage = () => {
-  const [selectedData, setSelectedData] = useState({});
 
-
-  return (
-    <Layout>
-      <div style={{ padding: '1rem' }}>
-        <h1 style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '1rem' }}>PC Builder</h1>
-        {featuredCategories.map((category) => (
-          <Card key={category.key} style={{ marginBottom: '1rem' }}>
-            <div>
-			<div className="flex w-full items-center space-x-2 sm:space-x-4">
-				<img className=" object-cover w-16 h-16 sm:w-20 md:h-20 rounded outline-none" src={category.image} alt="Polaroid camera" />
-				<div className="flex flex-col justify-between w-full pb-4">
-					<div className="flex flex-col sm:flex-row items-center justify-between w-full pb-2 space-x-2">
-						<div className="space-y-1">
-							<h3 className=" text-base sm:text-lg font-semibold leadi sm:pr-8">{category.label}</h3>
-							<p className="text-sm bg-gray-400 h-4 w-36 md:w-56"></p>
-						</div>
-						<div className="mt-5 sm:text-right">
-							<Link href={`/categories/${category.key}`}>
-                            <button className='py-3 cursor-pointer px-10 rounded-md bg-blue-500 border-none text-white'>Add</button>
-                            </Link>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			{/* <div className="flex mt-5 w-full space-x-2 sm:space-x-4">
-				<img className="flex-shrink-0 object-cover w-20 h-20 dark:border-transparent rounded outline-none sm:w-32 sm:h-32 dark:bg-gray-500" src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="Polaroid camera" />
-				<div className="flex flex-col justify-between w-full pb-4">
-					<div className="flex justify-between w-full pb-2 space-x-2">
-						<div className="space-y-1">
-							<h3 className="text-lg font-semibold leadi sm:pr-8">Polaroid camera</h3>
-							<p className="text-sm dark:text-gray-400">Classic</p>
-						</div>
-						<div className="text-right">
-							<p className="text-lg font-semibold">59.99€</p>
-							<p className="text-sm line-through dark:text-gray-600">75.50€</p>
-						</div>
-					</div>
-				</div>
-			</div> */}
-            </div>
-          </Card>
-        ))}
-        <Button type="primary" style={{ width: '100%' }}>
-          Build PC
-        </Button>
+    return (
+      <div>
+        <MyPcBuild />
       </div>
-    </Layout>
-  );
+    );
 };
 
 export default PCBuildPage;
 
-
-
 PCBuildPage.getLayout = function getLayout(page) {
     return <RootLayout>{page}</RootLayout>
-  }
-  
+}
