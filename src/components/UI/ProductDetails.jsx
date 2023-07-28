8c2c75ede9060e03703ece70ee588e5d5425bb0f
Nur Mohammad <mohammadnur5480@gmail.com>
2023-07-28 14:37:02 +0600

    Create category

--- src/components/UI/ProductDetails.jsx
+++ src/components/UI/ProductDetails.jsx
@@ -1,25 +1,30 @@
-import { StarFilled } from '@ant-design/icons';
-import { Divider } from 'antd';
+import { StarFilled } from "@ant-design/icons";
+import { Divider } from "antd";
 
 const ProductDetails = ({ product }) => {
   return (
     <div className="p-4">
+      <div className="flex flex-col md:flex-row items-center justify-center">
+        <div className="flex w-[50%] justify-center">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="w-64 h-64 object-cover"
+          />
+        </div>
+        <div className="my-4">
+          <h2 className="text-2xl font-bold mb-2">{product.name}</h2>
+          <p className="text-gray-500 mb-2">{product.category}</p>
+          <p className="text-gray-500 mb-2">
+            {product.status ? "In Stock" : "Out of Stock"}
+          </p>
+          <p className="text-2xl font-bold mb-2">${product.price}</p>
 
-     <div className='flex flex-col md:flex-row items-center justify-center'>
-     <div className="flex w-[50%] justify-center">
-        <img src={product.image} alt={product.name} className="w-64 h-64 object-cover" />
+          <h3 className="text-lg font-bold mb-2">Description</h3>
+          <p>{product.description}</p>
+        </div>
       </div>
-      <div className="my-4">
-        <h2 className="text-2xl font-bold mb-2">{product.name}</h2>
-        <p className="text-gray-500 mb-2">{product.category}</p>
-        <p className="text-gray-500 mb-2">{product.status ? 'In Stock' : 'Out of Stock'}</p>
-        <p className="text-2xl font-bold mb-2">${product.price}</p>
-     
-        <h3 className="text-lg font-bold mb-2">Description</h3>
-        <p>{product.description}</p>
-      </div>
-     </div>
-     <Divider>Features</Divider>
+      <Divider>Features</Divider>
       <div className="my-4">
         <h3 className="text-lg font-bold mb-2">Key Features</h3>
         <ul className="list-disc pl-6">
